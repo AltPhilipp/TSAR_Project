0ac03fa70e90e143dd395383fcdbf2e472d90e3d
AltPhilipp <philipp.altenbach@students.fhnw.ch>
2025-02-27 11:21:50 +0100

    Progress

--- EDA/Working_File.R
+++ EDA/Working_File.R
@@ -1,8 +1,3 @@
----
-title: "P1-5-Grieder_Kreis_Brinis_Altenbach"
-format: html
----
-
 # install the required packages if needed
 if (!require("data.table")) install.packages("data.table")
 if (!require("dplyr")) install.packages("dplyr")
@@ -10,11 +5,9 @@ if (!require("dplyr")) install.packages("dplyr")
 library(data.table)
 library(dplyr)
 
-LCdata <- fread("Data/LCdata.csv")
-set.seed(5)
-myLCdata <- LCdata %>% sample(5) %>% slice_sample(prop = .5)
-fwrite(myLCdata, file = "Data/myLCdata.csv")
+# LCdata <- fread("Data/LCdata.csv")
+# set.seed(5)
+# myLCdata <- LCdata %>% sample(5) %>% slice_sample(prop = .5)
+# fwrite(myLCdata, file = "Data/myLCdata.csv")
 
 myLCdata <- fread("Data/myLCdata.csv")
-
-
